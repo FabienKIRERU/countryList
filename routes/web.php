bce9f16fa8d2421928c5e91af4f67071ddcfdbdd
FabienKIRERU <fabien02kaly@gmail.com>
2025-03-27 19:01:57 +0100

    fix:UrlWelcome

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\CountryController;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return Inertia::render('welcome');
+// })->name('home');
 
-Route::get('/hello', function () {
-    return Inertia::render('Hello', [
-        'message' => 'Salut toi, Welcome to Inertia avec React'
-    ]);
-});
-
-Route::get('/countries', [CountryController::class, 'index']);
+Route::get('/', [CountryController::class, 'index']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
